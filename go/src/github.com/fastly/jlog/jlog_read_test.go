e5483b3ee0942aef945ce27385d571fc4ede17fa
twmb <travis.bischel@gmail.com>
2013-12-05 23:53:44 +0000

    demonstrate mmaped data fails and copy does not
    See comments in file for exact process of when it'd fail.

--- go/src/github.com/fastly/jlog/jlog_read_test.go
+++ go/src/github.com/fastly/jlog/jlog_read_test.go
@@ -7,18 +7,31 @@ import (
 )
 
 func TestReading(t *testing.T) {
+	writeCount := 1000000
 	initialize(t)
 
 	writer, _ := jlog.NewWriter(pathname, nil)
 	writer.AddSubscriber("reading", jlog.BEGIN)
-	usageWritePayloads(pcnt, t) // write to the jlog
-
-	reader, _ := jlog.NewReader(pathname, nil)
-	e := reader.Open("reading")
+	writer.AddSubscriber("readlater", jlog.BEGIN)
+	e := writer.Open()
 	if e != nil {
-		t.Errorf("Unable to open reader, error %v", reader.ErrString())
+		t.Errorf("cannot open for writing, %v", writer.ErrString())
+	}
+	for i := 0; i < writeCount; i++ {
+		writer.Write([]byte("hello"))
 	}
 
+	// Open both readers. They will both point to the start
+	reader, _ := jlog.NewReader(pathname, nil)
+	reader.Open("reading")
+
+	readerLaterer, _ := jlog.NewReader(pathname, nil)
+	readerLaterer.Open("readlater")
+	// Read once from the one that will be "saved" later
+	// (goroutine switches from this goroutine to next *right* after read)
+	latererBytesRead, _ := readerLaterer.Read()
+
+	// Read from the other goroutine, consume everything in logs and force a munumap
 	bytes, e := reader.Read()
 	log.Printf("string: %v", string(bytes))
 	for {
@@ -27,11 +40,16 @@ func TestReading(t *testing.T) {
 			break
 		}
 	}
+	for i := 0; i < writeCount; i++ {
+		writer.Write([]byte("goodbye"))
+	}
 
-	usageWritePayloads(pcnt, t) // write to the jlog
-
+	// Read again from other gorotuine. The original bytes would've been saved by now, but latererBytesRead isn't.
 	bytes2, e := reader.Read()
 	log.Printf("string2: %v", string(bytes2))
 	log.Printf("string: %v", string(bytes))
-	log.Printf("if not equal, either the perl bindings have a problem or jlog has a problem")
+	// Expected they wouldn't be equal.
+
+	// This needs to be "hello"
+	log.Printf("nowRead: %v", string(latererBytesRead))
 }
